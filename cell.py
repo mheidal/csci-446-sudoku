abef7e9546d35ee513356a1775ba9c8ce6c780c1
linknmike <mikeheidal@gmail.com>
2021-09-12 16:25:02 -0600

    Created constructor for Cell

--- cell.py
+++ cell.py
@@ -6,3 +6,12 @@ class Cell:
     location: List[int] = {int, int}
     value: int = 0
     possible_values: List[int] = None
+
+    def __init__(self, loc, val = 0):
+        self.location = loc
+        self.value = val
+        return
+
+    def set_possible_values(self, poss) -> None:
+        self.possible_values = poss
+        return
