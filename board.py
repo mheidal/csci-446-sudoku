6c43219197c160ebfdb3261357ac50e017c36d39
linknmike <mikeheidal@gmail.com>
2021-09-12 17:54:06 -0600

    board and cell quality of life, forward checking bare bones

--- board.py
+++ board.py
@@ -1,7 +1,22 @@
 from enum import Enum
 from typing import List
 
-import np as np
+import numpy as np
+
+# Diagram of the board:
+#
+#        0 1 2 3 4 5 6 7 8
+#        _ _ _ _ _ _ _ _ _
+#     0 |_|_|_|_|_|_|_|_|_|
+#     1 |_|0|_|_|1|_|_|2|_|
+#     2 |_|_|_|_|_|_|_|_|_|
+#     3 |_|_|_|_|_|_|_|_|_|
+#     4 |_|3|_|_|4|_|_|5|_|
+#     5 |_|_|_|_|_|_|_|_|_|
+#     6 |_|_|_|_|_|_|_|_|_|
+#     7 |_|6|_|_|7|_|_|8|_|
+#     8 |_|_|_|_|_|_|_|_|_|
+
 
 from cell import Cell
 
@@ -26,3 +41,28 @@ class Board:
 
     def check_success(self) -> Status:
         pass
+
+    def get_cells_in_box(self, index: int) -> List[Cell]:
+        rows = []
+        cols = []
+        box = []
+        if int(index / 3) == 0:
+            rows = [0, 1, 2]
+        elif int(index / 3) == 1:
+            rows = [3, 4, 5]
+        elif int(index / 3) == 2:
+            rows = [6, 7, 8]
+
+        if int(index % 3) == 0:
+            cols = [0, 1, 2]
+        elif int(index % 3) == 1:
+            cols = [3, 4, 5]
+        elif int(index % 3) == 2:
+            cols = [6, 7, 8]
+
+        for row in rows:
+            for col in cols:
+                #TODO: I DON'T KNOW HOW TO ACCESS GRID; WHAT TYPE IS IT?
+                box.append(self.grid[row][col])
+
+        return box
--- cell.py
+++ cell.py
@@ -15,3 +15,33 @@ class Cell:
     def set_possible_values(self, poss) -> None:
         self.possible_values = poss
         return
+
+    def get_row_index(self) -> int:
+        return self.location[0]
+
+    def get_col_index(self) -> int:
+        return self.location[1]
+
+#TODO: THERE'S A MORE ELEGANT WAY TO DO THIS
+    def get_box_index(self) -> int:
+        if self.location[0] % 3 == 0:
+            if self.location[1] % 3 == 0:
+                return 0
+            elif self.location[1] % 3 == 1:
+                return 1
+            elif self.location[1] % 3 == 2:
+                return 2
+        if self.location[0] % 3 == 1:
+            if self.location[1] % 3 == 0:
+                return 3
+            elif self.location[1] % 3 == 1:
+                return 4
+            elif self.location[1] % 3 == 2:
+                return 5
+        if self.location[0] % 3 == 2:
+            if self.location[1] % 3 == 0:
+                return 6
+            elif self.location[1] % 3 == 1:
+                return 7
+            elif self.location[1] % 3 == 2:
+                return 8
