d4a7711e45e170a601b3787e59be4b0176d944cc
wward <woward00@gmail.com>
2021-09-14 01:14:03 -0600

    Board.value functional. Not complete until Board.get_cells_in_block() is complete

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ from board import Board
 
 def main():
     board: Board = Board()
-    print(board.value)
+    row = board.row(board[3][4])
+    column = board.column(board[3][4])
+
+    print(f"board[3][4]:\t{board[3][4]}")
+    print(f"row[0]:\t\t\t{row[0]}")
+    print(f"column[0]:\t\t{column[0]}")
+    print(f"board.value:\t{board.value}")
     pass
 
 
