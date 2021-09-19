b08df0a0e463b0ba0472ce82ed6b6b91b8cb007e
Wildcat_Steamer <55596289noah-t-cunningham@users.noreply.github.com>
2021-09-19 16:52:09 -0600

    complete ga

--- local_search_genetic_penalty_fx_tourny_selection_constraint_solver.py
+++ local_search_genetic_penalty_fx_tourny_selection_constraint_solver.py
@@ -6,11 +6,21 @@ import copy
 import random
 
 B : Board = Board()
+popsize: int = 30
+
+def random_board(board: Board):
+    temp_board = copy.deepcopy(board)
+    for row in range(9):
+        for col in range(9):
+            if temp_board.grid[row][col].value == 0:
+                random_number = random.randint(1, 9)
+                temp_board.grid[row][col].value = random_number
+    return temp_board
 
 def population(board: Board) -> list:
     pop = []
-    popsize: int = 25
     for i in range(popsize):
+        #temp_board = random_board(board)
         temp_board = copy.deepcopy(board)
         for row in range(9):
             for col in range(9):
@@ -30,7 +40,7 @@ def fitness(pop: list):
     for i in pop:
         fit_dict[i] = i.value
         #fit_prob_list.append((i.value/total_fit) * 100)
-    # print(fit_dict)
+    #print(fit_dict)
     # print(fit_prob_list)
     return fit_dict
    #tournamnet_selection(fit_prob_list, pop, fit_dict)
@@ -42,18 +52,23 @@ def tournamnet_selection(fit_dict: dict):
     total_fit = sum(fit_list)
     fit_prob_list = []
     for i in range(len(fit_list)):
-        fit_prob_list.insert(i, ((1/fit_list[i])/total_fit) * 100)
+        fit_prob_list.insert(i, ((1/fit_list[i])/total_fit) * 1000000)
+    #print("fit_prop_list",fit_prob_list)
+    #print("fit_list", fit_list)
     combatant = random.choices(pop, weights=fit_prob_list, k=1)[0]
     gladiator = random.choices(pop, weights=fit_prob_list, k=1)[0]
+
+    # print("Combatant", combatant.value, "gladiator", gladiator.value)
     # while loop that makes sure combatant and gladiator are differant boards
-    while(combatant.value == gladiator.value):
+    while combatant.value == gladiator.value:
         gladiator = random.choices(pop, weights=fit_prob_list, k=1)[0]
-    if gladiator.value > combatant.value:
+    if gladiator.value < combatant.value:
         return gladiator
     else:
         return combatant
+    #return combatant
 
-def crossover(mom: Board, dad: Board) -> Board:
+def crossover(dad: Board, mom: Board) -> Board:
     child_1: Board = Board()
     child_2: Board = Board()
     #crossover point
@@ -62,23 +77,87 @@ def crossover(mom: Board, dad: Board) -> Board:
 
     for row in range(9):
         for col in range(9):
-            if (row <= random_row and col <= random_col):
-                mom[row][col].value = child_1[row][col].value
-                dad[row][col].value = child_2[row][col].value
+            if row <= random_row and col <= random_col:
+                child_1[row][col].value = mom[row][col].value
+                child_2[row][col].value = dad[row][col].value
             else:
-                mom[row][col].value = child_1[row][col].value
-                dad[row][col].value = child_2[row][col].value
-    # print(mom.__str__())
-    # dad.__str__()
-    # child_1.__str__()
-    # child_2.__str__()
+                child_2[row][col].value = mom[row][col].value
+                child_1[row][col].value = dad[row][col].value
+    # print("Mom",mom.__str__())
+    # print("Dad",dad.__str__())
+    # print("Child 1", child_1.__str__())
+    # print("Child 2",child_2.__str__())
+    return [child_1, child_2]
+
+def mutate(children: list):
+    mutation_rate = 1/81
+    child_1 = children[0]
+    child_2 = children[1]
+    # print("before")
+    # print(child_1.__str__())
+    # print(child_2.__str__())
+    for row in range(9):
+        for col in range(9):
+            if random.random() < mutation_rate:
+                if not child_1[row][col].preset:
+                    child_1[row][col].value = random.randint(1,9)
+    for row in range(9):
+        for col in range(9):
+            if random.random() < mutation_rate:
+                if not child_1[row][col].preset:
+                    child_2[row][col].value = random.randint(1,9)
+    # print("after")
+    # print(child_1.__str__())
+    # print(child_2.__str__())
+    return [child_1, child_2]
 
 
 
-pop_val = []
+
+def replace(pop_val: list):
+    new_pop = []
+    while len(new_pop) < int(popsize * 0.8):
+        dad = tournamnet_selection(fit_dict)
+        mom = tournamnet_selection(fit_dict)
+        while (dad.value == mom.value):
+            mom = tournamnet_selection(fit_dict)
+        children = crossover(dad, mom)
+        children = mutate(children)
+        new_pop.append(children[0])
+        new_pop.append(children[1])
+    n = popsize - len(new_pop)
+    for i in range(n):
+        rand_int = random.randint(0, len(pop) - 1)
+        #print(pop_val.index(min(pop_val)))
+        #print("1",pop_val)
+        # new_pop.append(pop[pop_val.index(min(pop_val))])
+        # del pop_val[pop_val.index(min(pop_val))]
+        # del pop[0]
+        #print("2",pop_val)
+        new_pop.append(pop[rand_int])
+        #new_pop.append(random_board(B))
+    return new_pop
+
+def update_pop_val(pop: list):
+    pop_val = []
+    for i in range(len(pop)):
+        pop_val.insert(i, pop[i].value)
+    return pop_val
+
+
+gen = 0
 pop = population(B)
-for i in range(len(pop)):
-    pop_val.insert(i, pop[i].value)
+pop_val = update_pop_val(pop)
+
+while(min(pop_val) != 0):
+    fit_dict = fitness(pop)
+    pop = replace(pop_val)
+    pop_val = update_pop_val(pop)
+    #print(min(pop_val))
+    gen = gen + 1
+    #print("gen", gen)
+print("done")
+print(pop[pop_val.index(0)].__str__(), gen)
 
 # while(min(pop_val) != 0):
 #     fit_dict = fitness(pop)
@@ -88,9 +167,17 @@ for i in range(len(pop)):
 #         mom = tournamnet_selection(fit_dict)
 #     child = crossover(dad, mom)
 
-fit_dict = fitness(pop)
-dad = tournamnet_selection(fit_dict)
-mom = tournamnet_selection(fit_dict)
-while(dad.value == mom.value):
-    mom = tournamnet_selection(fit_dict)
-child = crossover(dad, mom)
+# fit_dict = fitness(pop)
+# dad = tournamnet_selection(fit_dict)
+# mom = tournamnet_selection(fit_dict)
+# # print("Mom",mom.__str__())
+# # print("Dad",dad.__str__())
+# while(dad.value == mom.value):
+#     mom = tournamnet_selection(fit_dict)
+# children = crossover(dad, mom)
+# children = mutate(children)
+# #pop = replace()
+
+
+
+
