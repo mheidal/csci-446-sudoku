3e69cafad0f28cac779466e5faa12e9ccff111a1
wward <woward00@gmail.com>
2021-09-19 18:53:50 -0600

    docs for simulated annealing; docs for board; modifications to main.py

--- local_search_simulated_annealing_minimum_conflict_constraint_solver.py
+++ local_search_simulated_annealing_minimum_conflict_constraint_solver.py
@@ -32,7 +32,7 @@ class LocalSearchSimulatedAnnealingMinimumConflictConstraintSolver(ConstraintSol
         Implements Simulated Annealing to solve the board Board.
         :param board: A unsolved sudoku board as a Board
         :param _threading: A bool to determine whether or not to implement multithreading for this method call.
-        :return:
+        :return: bool representing the success of solving board before the Temperature is less than 1 or the preset number of iterations is reached. If a solution is found returns True otherwise False.
         """
         print(self.__class__.__name__)
         if threading:
@@ -62,40 +62,22 @@ class LocalSearchSimulatedAnnealingMinimumConflictConstraintSolver(ConstraintSol
             self.solution = self.simulated_annealing(deepcopy(board))
             return True if self.solution.value == 0 else False
 
-    @staticmethod
-    def starting_board(_board: Board) -> Board:
-        board = deepcopy(_board)
-        for block in board.get_box_list():
-            domain: List[int] = deepcopy(board.domain)
-            not_preset_cells: List[Cell] = []
-            for cell in block:
-                if cell.preset:
-                    domain.remove(cell.value)
-                else:
-                    not_preset_cells.append(cell)
-            random.shuffle(not_preset_cells)
-            for cell in not_preset_cells:
-                cell.value = random.choice(domain)
-                domain.remove(cell.value)
-        return board
-
     def schedule(self, iteration_number: int) -> float:
-        tau: float = 99  # controls curvature of the graph of the temperature over iterations
+        """
+        Cooling Schedule for Simulated Annealing.
+        :param iteration_number: current iteration from Simulated Annealing.
+        :return:
+        """
+        tau: float = 99
         return (self.starting_temperature * tau) / (tau + iteration_number)
-
-    @staticmethod
-    def simple_schedule(current_temperature: float):
-        alpha: float = 0.5
-        return current_temperature * alpha
 
     def simulated_annealing(self, board: Board) -> Board:
         """
-        :param board:
-        :return:
+        Implementation of Simulated Annealing
+        :param board: Unsolved Sudoku board represented as a Board
+        :return: Board as a solution. If the Board is not solves, returns current progress.
         """
-        # k_boltzmann_constant: float = 1.0
         k_boltzmann_constant: float = 0.00001
-        # k_boltzmann_constant: float = 1.38*pow(10, -23)
         current_board = board
         for t in range(1, self.number_iterations):
             temperature: float = self.schedule(t)
@@ -115,105 +97,19 @@ class LocalSearchSimulatedAnnealingMinimumConflictConstraintSolver(ConstraintSol
                     next_board_probability = 0.01
                     current_board = random.choices([current_board, next_board], weights=[1, next_board_probability * 100], k=1)[0]
 
-    def simulated_annealing_v2(self, board: Board) -> Board:
-        k_boltzmann_constant: float = 0.00001
-        current_board = self.starting_board(board)
-        t: int = 0
-        temperature: float = self.starting_temperature
-        # print(current_board)
-        while t >= 0:
-            # temperature = self.simple_schedule(temperature) if t > 0 else temperature
-            temperature = self.schedule(t)
-            if temperature <= 1 or current_board.value == 0 or t > self.number_iterations:
-                self.solutions.append(deepcopy(current_board))
-                # print(
-                #     f"\nSimulated Annealing\nViolated Constraints: {current_board.value}\nNumber of Iterations: {t}\nCurrent Temperature: {temperature}\n")
-                return current_board  # if current_board.value == 0 else self.simulated_annealing(deepcopy(current_board))
-            next_board: Board = self.random_neighbor_v2(current_board)
-            delta_energy: int = (next_board.value - current_board.value) * -1
-            if delta_energy > 0:  # if the number of violated constrains in current is greater than that in next
-                current_board = next_board
-            elif delta_energy != 0:
-                next_board_probability: float = 0
-                try:
-                    next_board_probability = 1 / (
-                        exp((-delta_energy) / (k_boltzmann_constant * temperature)))
-                except OverflowError:
-                    next_board_probability = 0.01
-                # next_board_probability: float = 0 if (delta_energy == 0) and (temperature < self.starting_temperature/2) else 1 / (exp((-delta_energy) / (k_boltzmann_constant * temperature)))
-                current_board = \
-                    random.choices([current_board, next_board], weights=[1, next_board_probability * 100], k=1)[0]
-            t += 1
-
-    def simulated_annealing_v3(self, board: Board) -> Board:
+    @staticmethod
+    def random_neighbor(current_board) -> Board:
         """
-        :param board:
-        :return:
+        Static method used to randomly change the value of one cell in current board.
+        :param current_board: Current state of a Sudoku board represented as a Board from Simulated Annealing
+        :return: Next possible board for Simulated Annealing
         """
-        # k_boltzmann_constant: float = 1.0
-        k_boltzmann_constant: float = 0.00001
-        # k_boltzmann_constant: float = 1.38*pow(10, -23)
-        current_board = self.starting_board(board)
-        # print(current_board)
-        # print(current_board.value)
-        for t in range(1, self.number_iterations):
-            temperature: float = self.schedule(t)
-            if temperature < 0 or t == self.number_iterations - 1 or current_board.value == 0:
-                self.solutions.append(deepcopy(current_board))
-                # print(
-                #     f"\nSimulated Annealing\nViolated Constraints: {current_board.value}\nNumber of Iterations: {t}\nCurrent Temperature: {temperature}\n")
-                return current_board  # if current_board.value == 0 else self.simulated_annealing(deepcopy(current_board))
-            next_board: Board = self.random_neighbor(current_board)
-            delta_energy: int = (next_board.value - current_board.value) * -1
-            if delta_energy > 0:  # if the number of violated constrains in current is greater than that in next
-                current_board = next_board
-            elif delta_energy != 0:
-                next_board_probability: float = 0
-                try:
-                    next_board_probability = 1 / (
-                        exp((-delta_energy) / (k_boltzmann_constant * temperature)))
-                except OverflowError:
-                    next_board_probability = 0.01
-                # next_board_probability: float = 0 if (delta_energy == 0) and (temperature < self.starting_temperature/2) else 1 / (exp((-delta_energy) / (k_boltzmann_constant * temperature)))
-                current_board = \
-                    random.choices([current_board, next_board], weights=[1, next_board_probability * 100], k=1)[0]
-
-    def random_neighbor(self, current_board) -> Board:
         preselected: bool = True
         new_board: Board = deepcopy(current_board)
-        # new_board.grid = deepcopy(current_board.grid)
         while preselected:
             cell: Cell = deepcopy(current_board.grid[randrange(9)][randrange(9)])
             if not cell.preset and new_board.cell_value(cell) != 0:
                 preselected = False
-                orig_val: int = cell.value
                 cell.value = random.choice(cell.possible_values)
                 new_board.grid[cell.location[0]][cell.location[1]] = cell
-                # if self.debug is True:
-                #     print(f"Cell at [{cell.location[0]}][{cell.location[1]}] changed from {orig_val} to {cell.value}")
-        return new_board
-
-    def random_neighbor_v2(self, current_board) -> Board:
-        preselected: bool = True
-        new_board: Board = deepcopy(current_board)
-        cell: Cell = Cell((0, 0))
-        while preselected:
-            cell = current_board.grid[randrange(9)][randrange(9)]
-            if not cell.preset:
-                preselected = False
-        block = current_board.block(cell)
-        block.remove(cell)
-        cell = deepcopy(cell)
-        other_cell: Cell = deepcopy(random.choice(block))
-        temp_cell: Cell = deepcopy(other_cell)
-        # if self.debug is True:
-        #     print(f"Cell at [{cell.location[0]}][{cell.location[1]}]: {cell}")
-        #     print(f"Cell at [{other_cell.location[0]}][{other_cell.location[1]}]: {other_cell}")
-        new_board.grid[other_cell.location[0]][other_cell.location[1]].value = cell.value
-        new_board.grid[cell.location[0]][cell.location[1]].value = temp_cell.value
-        # if self.debug is True:
-        #     print(
-        #         f"Cell at [{cell.location[0]}][{cell.location[1]}]: {new_board.grid[temp_cell.location[0]][temp_cell.location[1]]}")
-        #     print(
-        #         f"Cell at [{temp_cell.location[0]}][{temp_cell.location[1]}]: {new_board.grid[cell.location[0]][cell.location[1]]}")
         return new_board
--- main.py
+++ main.py
@@ -1,15 +1,58 @@
+from typing import List
+
 from board import Board
-from cell import Cell
 from local_search_simulated_annealing_minimum_conflict_constraint_solver import \
     LocalSearchSimulatedAnnealingMinimumConflictConstraintSolver
 
 
 def main():
-    board: Board = Board()
-    simulated_annealing: LocalSearchSimulatedAnnealingMinimumConflictConstraintSolver = LocalSearchSimulatedAnnealingMinimumConflictConstraintSolver()
-    solved: bool = simulated_annealing.solve_csp(board=board, _threading=True)
-    pass
+    board_names: List[str] = ["Easy-P1", "Easy-P2", "Easy-P3", "Easy-P4", "Easy-P5",
+                              "Med-P1", "Med-P2", "Med-P3", "Med-P4",
+                              "Hard-P1", "Hard-P2", "Hard-P3", "Hard-P4", "Hard-P5",
+                              "Evil-P1", "Evil-P2", "Evil-P3", "Evil-P4", "Evil-P5"]
+    for board_file in board_names:
+        board: Board = Board(board_file_name=board_file)
+        simulated_annealing: LocalSearchSimulatedAnnealingMinimumConflictConstraintSolver = LocalSearchSimulatedAnnealingMinimumConflictConstraintSolver()
+        solved: bool = simulated_annealing.solve_csp(board=board, _threading=True)
 
 
 if __name__ == '__main__':
     main()
+
+
+# from board import *
+# from backtracking_constraint_solver import *
+# from simple_backtracking_constraint_solver import *
+# from backtracking_forward_checking_constraint_solver import *
+# from backtracking_arc_consistency_constraint_solver import *
+# from cell import *
+#
+# def test_constraint_solver(Constructor: ConstraintSolver):
+#     board_names = []
+#     if Constructor is not SimpleBacktracking:
+#         board_names = ["Easy-P1","Easy-P2","Easy-P3","Easy-P4","Easy-P5",
+#                       "Med-P1","Med-P2","Med-P3","Med-P4",
+#                        "Hard-P1","Hard-P2","Hard-P3","Hard-P4","Hard-P5",
+#                        "Evil-P1","Evil-P2","Evil-P3","Evil-P4","Evil-P5"]
+#     else:
+#         board_names = ["Easy-P1","Easy-P2","Easy-P3","Easy-P4","Easy-P5",
+#                       "Med-P1","Med-P2","Med-P3","Med-P4"]
+#
+#     for board_name in board_names:
+#         solver = Constructor()
+#         board = Board(board_file_name = board_name)
+#         print(board_name)
+#         if solver.solve_csp(board):
+#             print("Found solution.")
+#         else:
+#             print("Failed before finding solution.")
+#
+#         print()
+#         print("--------------------------------------")
+#         print()
+#     return
+#
+#
+# test_constraint_solver(ArcConsistency)
+# test_constraint_solver(ForwardChecking)
+# test_constraint_solver(SimpleBacktracking)
