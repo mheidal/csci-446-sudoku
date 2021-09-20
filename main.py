e1857f86cefb0dd96b80511e801ad60046fb485c
wward <woward00@gmail.com>
2021-09-20 15:25:16 -0600

    add med-5

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from local_search_simulated_annealing_minimum_conflict_constraint_solver import 
 
 def main():
     board_names: List[str] = ["Easy-P1", "Easy-P2", "Easy-P3", "Easy-P4", "Easy-P5",
-                              "Med-P1", "Med-P2", "Med-P3", "Med-P4",
+                              "Med-P1", "Med-P2", "Med-P3", "Med-P4", "Med-P5",
                               "Hard-P1", "Hard-P2", "Hard-P3", "Hard-P4", "Hard-P5",
                               "Evil-P1", "Evil-P2", "Evil-P3", "Evil-P4", "Evil-P5"]
     for board_file in board_names:
